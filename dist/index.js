7243de68c87a38cb162544125237ace51b8c18ec
Wessel <wesselvdp@gmail.com>
2020-04-08 13:12:20 +0200

    testrun

--- dist/index.js
+++ dist/index.js
@@ -394,20 +394,21 @@ const { accessKeyId, secretAccessKey } = process.env;
 const run = () => __awaiter(void 0, void 0, void 0, function* () {
     core.debug(projectJson);
 });
-run();
-// try {
-//   writeToServer()
-//   // `who-to-greet` input defined in action metadata file
-//   // const nameToGreet = core.getInput('who-to-greet')
-//   // console.log(`Hello ${nameToGreet}!`)
-//   // const time = new Date().toTimeString()
-//   // core.setOutput('time', time)
-//   // // Get the JSON webhook payload for the event that triggered the workflow
-//   // const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   // console.log(`The event payload: ${payload}`)
-// } catch (error) {
-//   core.setFailed(error.message)
-// }
+try {
+    run();
+    //   writeToServer()
+    //   // `who-to-greet` input defined in action metadata file
+    //   // const nameToGreet = core.getInput('who-to-greet')
+    //   // console.log(`Hello ${nameToGreet}!`)
+    //   // const time = new Date().toTimeString()
+    //   // core.setOutput('time', time)
+    //   // // Get the JSON webhook payload for the event that triggered the workflow
+    //   // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //   // console.log(`The event payload: ${payload}`)
+}
+catch (error) {
+    core.setFailed(error.message);
+}
 
 
 /***/ }),
--- src/index.ts
+++ src/index.ts
@@ -20,18 +20,20 @@ const { accessKeyId, secretAccessKey } = process.env
 const run = async (): Promise<void> => {
   core.debug(projectJson)
 }
-run()
-// try {
-//   writeToServer()
-//   // `who-to-greet` input defined in action metadata file
-//   // const nameToGreet = core.getInput('who-to-greet')
-//   // console.log(`Hello ${nameToGreet}!`)
 
-//   // const time = new Date().toTimeString()
-//   // core.setOutput('time', time)
-//   // // Get the JSON webhook payload for the event that triggered the workflow
-//   // const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   // console.log(`The event payload: ${payload}`)
-// } catch (error) {
-//   core.setFailed(error.message)
-// }
+try {
+  run()
+
+  //   writeToServer()
+  //   // `who-to-greet` input defined in action metadata file
+  //   // const nameToGreet = core.getInput('who-to-greet')
+  //   // console.log(`Hello ${nameToGreet}!`)
+
+  //   // const time = new Date().toTimeString()
+  //   // core.setOutput('time', time)
+  //   // // Get the JSON webhook payload for the event that triggered the workflow
+  //   // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   // console.log(`The event payload: ${payload}`)
+} catch (error) {
+  core.setFailed(error.message)
+}
