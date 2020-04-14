4b25d69001e48d8878e765591664067370739e31
Wessel <wesselvdp@gmail.com>
2020-04-14 13:23:38 +0200

    testrun

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
-// import github from '@actions/github'
-// import { writeToServer } from './helpers/writeToServer'
+// // import github from '@actions/github'
+// // import { writeToServer } from './helpers/writeToServer'
 import fs from 'fs'
 
 // Get project package.json
@@ -10,30 +10,29 @@ const projectJson = JSON.parse(projectPackageData.toString())
 const packageInfo = projectJson
 
 // Get project specific settings
-// const {
-//   deploySettings: { bucket, serverPath: path, buildFiles },
-//   name: projectName,
-// } = packageInfo
+const {
+  deploySettings: { bucket, serverPath: path, buildFiles },
+  name: projectName,
+} = packageInfo
 
-const { accessKeyId, secretAccessKey } = process.env
+// const { accessKeyId, secretAccessKey } = process.env
 
 const run = async (): Promise<void> => {
-  core.debug(projectJson)
+  console.log(projectJson)
+  // core.debug(projectJson)
 }
 
-try {
-  run()
-
-  //   writeToServer()
-  //   // `who-to-greet` input defined in action metadata file
-  //   // const nameToGreet = core.getInput('who-to-greet')
-  //   // console.log(`Hello ${nameToGreet}!`)
-
-  //   // const time = new Date().toTimeString()
-  //   // core.setOutput('time', time)
-  //   // // Get the JSON webhook payload for the event that triggered the workflow
-  //   // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //   // console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
-}
+// try {
+//   run()
+//   //   //   writeToServer()
+//   //   //   // `who-to-greet` input defined in action metadata file
+//   //   //   // const nameToGreet = core.getInput('who-to-greet')
+//   //   //   // console.log(`Hello ${nameToGreet}!`)
+//   //   //   // const time = new Date().toTimeString()
+//   //   //   // core.setOutput('time', time)
+//   //   //   // // Get the JSON webhook payload for the event that triggered the workflow
+//   //   //   // const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   //   //   // console.log(`The event payload: ${payload}`)
+// } catch (error) {
+//   // core.setFailed(error.message)
+// }
