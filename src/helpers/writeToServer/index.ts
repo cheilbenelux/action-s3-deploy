fe73db2344e2df4bcddced5159d5177c2d781af8
Wessel <wesselvdp@gmail.com>
2020-04-14 15:13:39 +0200

    testrun

--- src/helpers/writeToServer/index.ts
+++ src/helpers/writeToServer/index.ts
@@ -18,46 +18,51 @@ const endpoint: any = new AWS.Endpoint(`ams3.digitaloceanspaces.com`)
 // const { ACCESSKEYID, secretAccessKey } = require('./settings')
 const { ACCESSKEYID, SECRETACCESSKEY } = process.env
 
-const s3 = new AWS.S3({
-  endpoint,
-  accessKeyId: ACCESSKEYID,
-  secretAccessKey: SECRETACCESSKEY,
-})
-
 export const writeToServer = (
   projectOptions: ProjectOptions,
   staging: boolean,
 ) => {
-  const { bucket, projectName, path, buildFiles } = projectOptions
-  const streamOptions = {
-    partSize: 10 * 1024 * 1024, // 10 MB
-    queueSize: 10,
-  }
-
-  // Are the build files declared in the project? Then cherry pick! if not, get all css and js files.
-  const files = buildFiles
-    ? cherryPickFiles(buildFiles)
-    : [...getAllFiles('js'), ...getAllFiles('css')]
-
-  files.forEach((file) => {
-    console.log('key', formatKey(projectName, path, file, staging))
-    const ContentType =
-      file.type === 'css' ? 'text/css' : 'application/javascript'
-    const params = {
-      Bucket: bucket,
-      Key: formatKey(projectName, path, file, staging),
-      Body: fs.createReadStream(file.path),
-      ACL: 'public-read',
-      ContentType,
+  try {
+    const { bucket, projectName, path, buildFiles } = projectOptions
+    const streamOptions = {
+      partSize: 10 * 1024 * 1024, // 10 MB
+      queueSize: 10,
     }
-    s3.upload(params, streamOptions, (err, data) => {
-      if (err) {
-        console.log('Error', err)
-        process.exit(1)
-      }
-      if (data) {
-        console.log('Upload Success', data.Location)
-      }
+
+    const s3 = new AWS.S3({
+      endpoint,
+      accessKeyId: ACCESSKEYID,
+      secretAccessKey: SECRETACCESSKEY,
     })
-  })
+
+    // Are the build files declared in the project? Then cherry pick! if not, get all css and js files.
+    const files = buildFiles
+      ? cherryPickFiles(buildFiles)
+      : [...getAllFiles('js'), ...getAllFiles('css')]
+
+    files.forEach((file) => {
+      console.log('key', formatKey(projectName, path, file, staging))
+      const ContentType =
+        file.type === 'css' ? 'text/css' : 'application/javascript'
+      const params = {
+        Bucket: bucket,
+        Key: formatKey(projectName, path, file, staging),
+        Body: fs.createReadStream(file.path),
+        ACL: 'public-read',
+        ContentType,
+      }
+      s3.upload(params, streamOptions, (err, data) => {
+        if (err) {
+          console.log('Error', err)
+          process.exit(1)
+        }
+        if (data) {
+          console.log('Upload Success', data.Location)
+        }
+      })
+    })
+  } catch (error) {
+    console.log('oh no!', error)
+    process.exit(1)
+  }
 }
