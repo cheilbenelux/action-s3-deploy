aca85b3bb91f960e11e2f54a7ae880f4032ab6bd
Wessel van der Plas <wesselvdp@gmail.com>
2020-11-04 14:27:59 +0100

    added purge function

--- src/helpers/writeToServer/index.ts
+++ src/helpers/writeToServer/index.ts
@@ -1,8 +1,10 @@
 import fs from 'fs'
 import AWS from 'aws-sdk'
+import fetch from 'node-fetch'
 
 import { getAllFiles, cherryPickFiles } from './getFiles'
 import { formatKey } from './formatKey'
+import { String } from 'aws-sdk/clients/batch'
 
 interface ProjectOptions {
   bucket: string
@@ -11,7 +13,19 @@ interface ProjectOptions {
   buildFiles: string[]
 }
 
-export const writeToServer = (
+// We want to be able to purge the cache of the CDN after an upload.
+const purgeCache = (bucketId: string, DOTOKEN: String, projectName: String) => {
+  fetch(`https://api.digitalocean.com/v2/cdn/endpoints/${bucketId}/cache`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${DOTOKEN}`,
+    },
+    body: JSON.stringify({ files: [`src/${projectName}/`] }),
+  })
+}
+
+export const writeToServer = async (
   projectOptions: ProjectOptions,
   branch: string,
 ) => {
@@ -24,7 +38,7 @@ export const writeToServer = (
 
     const endpoint: any = new AWS.Endpoint(`ams3.digitaloceanspaces.com`)
 
-    const { ACCESSKEYID, SECRETACCESSKEY } = process.env
+    const { ACCESSKEYID, SECRETACCESSKEY, SEBNBUCKETID, DOTOKEN } = process.env
 
     const s3 = new AWS.S3({
       endpoint,
@@ -32,32 +46,42 @@ export const writeToServer = (
       secretAccessKey: SECRETACCESSKEY,
     })
 
+    if (!ACCESSKEYID || !SECRETACCESSKEY || !SEBNBUCKETID || !DOTOKEN) {
+      throw new Error('missing settings')
+    }
+
     // Are the build files declared in the project? Then cherry pick! if not, get all css and js files.
     const files = buildFiles
       ? cherryPickFiles(buildFiles)
       : [...getAllFiles('js'), ...getAllFiles('css')]
 
-    files.forEach((file) => {
-      const ContentType =
-        file.type === 'css' ? 'text/css' : 'application/javascript'
-      const params = {
-        Bucket: bucket,
-        Key: formatKey({ projectName, serverPath, file, branch }),
-        Body: fs.createReadStream(file.path),
-        ACL: 'public-read',
-        ContentType,
-      }
+    await Promise.all(
+      files.map((file) => {
+        const ContentType =
+          file.type === 'css' ? 'text/css' : 'application/javascript'
+        const params = {
+          Bucket: bucket,
+          Key: formatKey({ projectName, serverPath, file, branch }),
+          Body: fs.createReadStream(file.path),
+          ACL: 'public-read',
+          ContentType,
+        }
 
-      s3.upload(params, streamOptions, (err, data) => {
-        if (err) {
-          console.log('Error', err)
-          process.exit(1)
-        }
-        if (data) {
-          console.log('Upload Success', data.Location)
-        }
-      })
-    })
+        s3.upload(params, streamOptions, (err, data) => {
+          if (err) {
+            console.log('Error', err)
+            process.exit(1)
+          }
+          if (data) {
+            console.log('Upload Success', data.Location)
+          }
+        })
+      }),
+    )
+
+    // Purge
+    const res = await purgeCache(SEBNBUCKETID, DOTOKEN, projectName)
+    console.log('res', res)
   } catch (error) {
     console.log('oh no!', error)
     process.exit(1)
